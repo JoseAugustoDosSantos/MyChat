fda1c8eb36c270ef7bcfb9e77266dfdce7c761c0
JoseAugustoDosSantos <augusto03jose@gmail.com>
2025-03-10 17:13:20 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({
+    port: process.env.PORT
+})
+
+function onMessage(ws, data){
+    console.log(data.toString())
+    ws.send('Received!')
+}
+function onError(ws, err){
+    console.log(err)
+    ws.send('Error')
+}
+function onConnection(ws, req){
+    console.log('Connected')
+    ws.on('message', data => onMessage(ws, data) )
+    ws.on('error', err => onError(ws, err))
+}
+
+wss.on('connection', onConnection)
+
+console.log(`Websocket server is running at http://localhost:${process.env.PORT}`)
